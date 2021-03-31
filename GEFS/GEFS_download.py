98ccc32e457323670dc1ee442c796b88b325e29f
Yingkai Sha <yingkaisha@gmail.com>
2021-03-31 14:27:31 -0600

    update downloading script.

--- GEFS/GEFS_download.py
+++ GEFS/GEFS_download.py
@@ -7,24 +7,25 @@ import subprocess
 import urllib.request
 from datetime import datetime
 
+print("[{}] Starting GEFS_Download.py ...".format(datetime.strftime(datetime.now(), '%F %T %z')))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('date_str', help='date_str. e.g., 20100101')
 args = vars(parser.parse_args())
 
 dt_fmt = args['date_str']
-
 if dt_fmt == 'auto':
     # Get the current UTC datetime
     dt_fmt = datetime.strftime(datetime.utcnow(), '%Y%m%d')
+print("Downloading GEFS for {}".format(dt_fmt))
 
-print("Date: {}".format(dt_fmt))
-
-# if today's sub folder exist
-flag_path = os.path.isdir(target_path.format(dt_fmt))
-
-# if not exist, creat through subprocess.Popen
-if flag_path is False:
-    subprocess.Popen(['mkdir', target_path.format(dt_fmt)])
+# set up download directory
+target_dir = target_path.format(dt_fmt)
+if os.path.isdir(target_dir):
+    print("{} already exists".format(target_dir))
+else:
+    print("Creating {}".format(target_dir))
+    os.mkdir(target_dir)
 
 # downloading status
 status = 0
@@ -32,17 +33,18 @@ status = 0
 for lead_ in fcst_leads:
     
     filename_ = filename.format(lead_)
-    print("Accessing: {}".format(filename_))
+    print("Downloading {}".format(filename_))
     
     try:    
-        urllib.request.urlretrieve(url_fmt.format(dt_fmt, filename_), os.path.join(target_path.format(dt_fmt), filename_));
+        urllib.request.urlretrieve(url_fmt.format(dt_fmt, filename_), os.path.join(target_dir, filename_));
         status = 1
-    
     except:
         # if file access is not successful, terminate all downloads
-        print("NOT accessible: {}".format(filename_))
+        print("{} is not available".format(filename_))
         status = 0
         break;
         
-with open("GEFS_download.log", "w") as log_io:
+with open("{}/download.status".format(target_dir), "w") as log_io:
     log_io.write(str(status))
+
+print("[{}] GEFS_Download.py is done.".format(datetime.strftime(datetime.now(), '%F %T %Z')))
